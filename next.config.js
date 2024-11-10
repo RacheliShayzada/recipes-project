6e1387240b0ec9ed82bb7cc46ed8d61deddceb53
RacheliShayzada <054racheli@gmail.com>
2024-11-10 14:25:35 +0200

    changing the grid component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      PUBLIC_DB_CONNECTION: "mongodb+srv://ruthiedel866:xrDREgWKVZHY36SB@cluster0.ri87r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  }};
+  
+  module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
+import ShowRecipes from '@/components/ShowRecipes/ShowRecipes';
 import React from 'react';
-import RecipesCard, { Recipe } from '@/components/recipesCard/RecipesCard';
-
-const sampleRecipe: Recipe = {
-  name: "Spaghetti Bolognese",
-  imageUrl: "https://example.com/spaghetti.jpg",
-  category: ["Italian", "Pasta"],
-  ingredients: ["Spaghetti", "Ground beef", "Tomato sauce", "Onion", "Garlic"],
-  instructions: "Cook spaghetti. Sauté beef with onions and garlic. Add tomato sauce. Combine and serve.",
-  shortDescription: "A classic Italian pasta dish with rich meat sauce.",
-};
 
 function Home() {
   return (
     <div>
       <h1>Recipe List</h1>
-      <RecipesCard recipe={sampleRecipe} isFavorite={true} />
+      <ShowRecipes/>
     </div>
   );
 }
