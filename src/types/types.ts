b10d559ff569f1508f6b02dd0118dd62556579fe
RacheliShayzada <054racheli@gmail.com>
2024-11-11 10:10:20 +0200

    remove trash

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,17 @@
 export type Recipe = {
-    id: string;                // מ��הה מיו��ד למתכו��
-    name: string;               // שם המתכון
-    imageUrl: string;           // URL של התמונה
-    category: string[];           // קטגוריות של המתכון
-    ingredients: string[];      // רשימת מצרכים (מחרוזות)
-    instructions: string;       // הוראות הכנה
-    shortDescription: string;   // תאור קצר של המתכון
+    id: string;                
+    name: string;           
+    imageUrl: string; 
+    category: string[];   
+    ingredients: string[];  
+    instructions: string;    
+    shortDescription: string; 
   };
 
 
   export type Category={
-    id: string;                // מ��הה מיו��ד למתכו��
-    name: string;             // שם הק����ו��י��
+    id: string;   
+    name: string; 
   }
 
        
