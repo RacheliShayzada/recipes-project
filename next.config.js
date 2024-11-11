201b05a65013ab3781c8ecc0ff3935bed7bbf046
ehilel <ehilel@g.jct.ac.il>
2024-11-11 10:14:14 +0200

    remove

--- next.config.js
+++
@@ -1,7 +0,0 @@
-
-const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        PUBLIC_DB_CONNECTION: "mongodb+srv://ruthiedel866:{password}@cluster0.ri87r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",  }};
-  
-  module.exports = nextConfig;
