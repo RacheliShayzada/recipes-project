1950f21f1552ebb5aa91be16ab60f878c89eecd5
Esty Friedman <ehshpiner@gmail.com>
2024-11-14 09:31:36 +0200

    additions

--- src/components/recipesCard/RecipesCard.tsx
+++ src/components/recipesCard/RecipesCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Recipe } from '@/types/types';
 import styles from './RecipesCard.module.css';
 import { useDisplayStore } from '@/services/providers/DisplayRecipeProvider'
